b347d1e0dd1f4f854d0f25a33342595a6e6e7030
Zatking <117702923+Zatking@users.noreply.github.com>
2025-02-19 11:16:43 +0700

    uncomment

--- app/main.py
+++ app/main.py
@@ -1,17 +1,17 @@
 from fastapi import FastAPI
-# from router import brand_router,cate_router,product_router
-# from database.data import Base, engine
+from router import brand_router,cate_router,product_router
+from database.data import Base, engine
 
 # Tạo bảng trong database
-# Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="FastAPI CRUD Example with SQL Server")
 
 
-# # Đăng ký router
-# app.include_router(brand_router.router, prefix="/api", tags=["brands"])
-# app.include_router(cate_router.router, prefix="/api", tags=["categories"])
-# app.include_router(product_router.router, prefix="/api", tags=["products"])
+# Đăng ký router
+app.include_router(brand_router.router, prefix="/api", tags=["brands"])
+app.include_router(cate_router.router, prefix="/api", tags=["categories"])
+app.include_router(product_router.router, prefix="/api", tags=["products"])
 
 
 @app.get("/")
@@ -25,5 +25,5 @@ def Hello():
 
 @app.get("/VercelTest")
 def VercelTest():
-    return {"message": "Vercel Testing v2"}
+    return {"message": "Vercel Testing"}
 
